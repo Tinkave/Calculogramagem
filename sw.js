f5d50578706dc67568855b2bb733fada7329afdd
Tinkave <helder.lemos@tinkave.pt>
2026-06-11 17:23:57 +0100

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'tinkave-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['.', 'index.html'])));
+  self.skipWaiting();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return resp;
+    }).catch(() => caches.match('.')))
+  );
+});
